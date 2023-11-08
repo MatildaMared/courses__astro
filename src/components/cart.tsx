66017eb661f3f1573ec715408991e2d58afb41e7
Matilda Mared <matildamared@live.se>
2023-11-08 08:01:04 +0100

    work on cart component

--- src/components/cart.tsx
+++ src/components/cart.tsx
@@ -1,7 +1,7 @@
 import { useStore } from "@nanostores/solid";
 import { $cart, removeItemFromCart, subtotal } from "../stores/cart";
 import styles from "./cart.module.css";
-import { show, createSignal } from "solid-js";
+import { Show, createSignal } from "solid-js";
 
 function formatCurrency(amount: number) {
 	return new Intl.NumberFormat("en-US", {
@@ -9,3 +9,18 @@ function formatCurrency(amount: number) {
 		currency: "USD",
 	}).format(amount);
 }
+
+const EmptyState = () => {
+	return (
+		<>
+			<p class={styles.icon}>
+				<span role="img" aria-label="hot dog">
+					🌭
+				</span>
+			</p>
+			<p class={styles.empty}>
+				Your cart is empty! Add a sandwich kit or two and give flavor a change.
+			</p>
+		</>
+	);
+};
